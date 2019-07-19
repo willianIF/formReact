52bbe8a059133e0eae6a62217872ee71e25a0626
willianIF <30538522+willianIF@users.noreply.github.com>
2019-07-18 21:12:50 -0300

    Delete e Update

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
   }
   async componentDidMount(){
     this.refs.nome.focus();
-    const {data: datas} = await axios.get(formAPI);
-    return this.setState({datas});
+    this.listar();
   }
   async listar(){
-
+    const {data: datas} = await axios.get(formAPI);
+    return this.setState({datas});
   }
 
   cadastrar = (event) => {
@@ -31,29 +31,29 @@ class App extends Component {
     axios.post(formAPI, obj);
     this.refs.formTarefas.reset();
     this.refs.nome.focus();
-    this.listar();
+    this.listar();  
   }
 
   remover = (event, tarefas) => {
     event.preventDefault();
     axios.delete(formAPI + tarefas);
     this.refs.formTarefas.reset();
-    this.refs.nome.focus();
+    this.listar(); //Como resolver sem chamar duas vezes o listar?
     this.listar();
   }
 
-  editar = (tarefa) => {
-    let data = this.state.datas[tarefa];
-    this.refs.nome.value = data.nome;
-    this.refs.horaCriacaoTarefa.value = data.horaCriacaoTarefa;
-    this.refs.horaEntregaTarefa.value = data.horaEntregaTarefa;
-    this.refs.descricao.value = data.descricao;
+  editar = (event, tarefa) => {
+    event.preventDefault();
+    const obj = {
+      nome: this.refs.nome.value,
+      horaCriacaoTarefa: this.refs.horaCriacaoTarefa.value,
+      horaEntregaTarefa: this.refs.horaEntregaTarefa.value,
+      descricao: this.refs.descricao.value
+    }
+    axios.put(formAPI + tarefa, obj);
 
-    this.setState({
-      act: 1,
-      index: tarefa
-    });
     this.refs.nome.focus();
+    this.listar(); 
   }
 
   render(){
@@ -71,7 +71,7 @@ class App extends Component {
                   <div className="input-group-prepend">
                     <span className="input-group-text ligth-b" id="basic-addon1">Nome</span>
                   </div>
-                  <input type="text" className="form-control" name="nome" ref="nome" placeholder="Insira o Nome da tarefa" aria-label="nome" aria-describedby="basic-addon1" />
+                  <input required oninvalid="this.setCustomValidity('Campo requerido')" type="text" className="form-control" name="nome" ref="nome" placeholder="Insira o Nome da tarefa" aria-label="nome" aria-describedby="basic-addon1" />
                 </div>
               </div>
 
@@ -80,8 +80,8 @@ class App extends Component {
                   <div className="input-group-prepend">
                     <span className="input-group-text ligth-b" id="basic-addon1">Horario da criação e da entrega</span>
                   </div>
-                  <input type="time" className="form-control" name="horaCriacaoTarefa" ref="horaCriacaoTarefa" pattern="d{2}:d{2}" aria-label="horaCriacao" aria-describedby="basic-addon1" />
-                  <input type="time" className="form-control" name="horaIntregaTarefa" ref="horaEntregaTarefa" pattern="d{2}:d{2}" aria-label="horaEntrega" aria-describedby="basic-addon1" />
+                  <input required="required" type="time" className="form-control" name="horaCriacaoTarefa" ref="horaCriacaoTarefa" pattern="d{2}:d{2}" aria-label="horaCriacao" aria-describedby="basic-addon1" />
+                  <input required="" type="time" className="form-control" name="horaIntregaTarefa" ref="horaEntregaTarefa" pattern="d{2}:d{2}" aria-label="horaEntrega" aria-describedby="basic-addon1" />
                 </div>
               </div>
               <div className="row">
@@ -89,7 +89,7 @@ class App extends Component {
                   <div className="input-group-prepend">
                     <span className="input-group-text ligth-b" id="basic-addon1">Descrição</span>
                   </div>
-                  <textarea className="form-control" name="descricao" ref="descricao" placeholder="Descreva a tarefa que ira ser realizada" aria-label="descricao" aria-describedby="basic-addon1" />
+                  <textarea required="" className="form-control" name="descricao" ref="descricao" placeholder="Descreva a tarefa que ira ser realizada" aria-label="descricao" aria-describedby="basic-addon1" />
                 </div>
               </div>
               <div className="row justify-content-md-center">
@@ -99,10 +99,10 @@ class App extends Component {
                 </div>
             </form>    
             <pre>
-              {datas.map((data) =>
+              {datas.map((data, i) =>
               <ul className="list-group">
-                <li key={data.id} className="list-group-item col-sm">
-                  <h4><span className="badge badge-secondary color-red col-sm">Tarefa: {data.id} Nome: {data.nome}</span></h4>
+                <li key={i} className="list-group-item col-sm">
+                  <h4><span className="badge badge-secondary color-red col-sm">Tarefa: {i + 1} Nome: {data.nome}</span></h4>
                   <h5>Hora da criação: {data.horaCriacaoTarefa}</h5> 
                 </li>
 
@@ -115,7 +115,7 @@ class App extends Component {
                 </li>
                 <li className="list-group-item col-sm text-center">
                   <button type="button" className="btn btn-danger-list" onClick={(e)=>this.remover(e, data.id)}>Remover</button>
-                  <button type="button" className="btn btn-list" onClick={()=>this.editar(data.id)}>Editar</button>
+                  <button type="button" className="btn btn-list" onClick={(e)=>this.editar(e, data.id)}>Editar</button>
                 </li>
               </ul>
               )}
